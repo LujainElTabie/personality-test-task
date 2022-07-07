985163b35976eeaae1baa181d0366106963ba750
lujaineltabei <lujain2@gmail.com>
2022-07-07 23:57:33 +0200

    first commit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,80 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
+  reset,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  selectQuestion,
+  agree,
+  disagree,
+  selectEx,
+  selectIn,
+  selectInC,
+  selectExC,
+} from "./counterSlice";
+import styles from "./Counter.module.css";
+import { Link } from "react-router-dom";
 
 export function Counter() {
+
+  
+
+   
+
+  const newQuestion =()=>{
+    const rand =Math.floor(Math.random()*questionBank.length);
+    setQuestion(questionBank[rand])
+    setQuestionBank((products) => products.filter((_, index) => index !== rand))
+    
+  }
+  
+  
   const count = useSelector(selectCount);
+  const questions = useSelector(selectQuestion);
+  
+  const introvert = useSelector(selectIn);
+  const extrovert = useSelector(selectEx);
+  const introvertC = useSelector(selectInC);
+  const extrovertC = useSelector(selectExC);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [questionCount, setQuestionCount] = useState(0);
+  const [question, setQuestion] = useState();
+  const [questionBank, setQuestionBank] = useState(questions);
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   const incrementValue = Number(incrementAmount) || 0;
-
+  
+  useEffect(() => {
+    newQuestion();
+  }, [])
   return (
     <div>
+      {questionCount<5?<div><div className={styles.row}>
+        
+        <div>{question?.text}</div>
+      </div>
       <div className={styles.row}>
         <button
           className={styles.button}
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
+          onClick={() => {dispatch(agree()); setQuestionCount(questionCount+1); newQuestion()}}
         >
-          -
+          agree
+       
         </button>
-        <span className={styles.value}>{count}</span>
         <button
           className={styles.button}
           aria-label="Increment value"
-          onClick={() => dispatch(increment())}
+          onClick={() => {dispatch(disagree()); setQuestionCount(questionCount+1); newQuestion()}}
         >
-          +
+          disagree
+          {console.log("hi"+introvertC)}
         </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
+      </div></div>:<div>{introvert>extrovert?"Introvert":"Extrovert"}
+      
+      
+      <button onClick={()=>{setQuestionCount(0); dispatch(reset(introvert>extrovert?"Introvert":"Extrovert")); setQuestionBank(questions)}}>Again</button></div>}
+      <Link onClick={()=>{dispatch(reset(introvert>extrovert?{per:"Introvert",score:introvert}:{per:"Extrovert", score:extrovert})); }} to="/">home</Link>
+
     </div>
   );
 }
